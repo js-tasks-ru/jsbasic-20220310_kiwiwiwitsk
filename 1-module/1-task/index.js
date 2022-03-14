b996ae91f3512c6ee7b927d7218fc6b514565afd
NikiforowN <kiwiwiwitsk@gmail.com>
2022-03-15 02:22:07 +0700

    1-module\1-task done

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,11 @@
 function factorial(n) {
-  // ваш код...
+  if (n === 0 || n === 1) {
+    return 1;
+  } else {
+    let fact = n;
+    for (let i = 1; i < n; i++) {
+      fact *= (n - i)
+    }
+    return fact;
+  }
 }
