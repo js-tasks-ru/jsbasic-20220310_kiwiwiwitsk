806079ff4ba7fc15ff2ee475877309be631d26a7
NikiforowN <kiwiwiwitsk@gmail.com>
2022-03-24 03:05:05 +0700

    4-module\2-task done

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,7 @@
 function makeDiagonalRed(table) {
-  // ваш код...
+  // Цикл пройдет по всем рядам таблицы
+  for (let i = 0; i < table.rows.length; i++) {
+    // Если номер ряда соответствует номеру ячейки - её цвет изменится на красный
+    table.rows[i].cells[i].style.background = 'red';
+  }
 }
