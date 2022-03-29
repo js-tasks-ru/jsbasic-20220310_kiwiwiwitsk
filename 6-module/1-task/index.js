312bdd0292f9c1d9839a4a3e2099fbec9df28d31
NikiforowN <kiwiwiwitsk@gmail.com>
2022-03-29 22:09:20 +0700

    6-module\1-task done

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -13,6 +13,58 @@
  *
  */
 export default class UserTable {
+  // Свойство elem
+  elem = null;
+
   constructor(rows) {
+    this.rows = rows;
+    // elem присвоена ссылка на table
+    this.elem = document.createElement('table');
+    // Вызов функции, заполняищий table
+    this.tableFiller();
+  };
+
+  // Геттер вернет ссылку на table
+  get elem() {
+    return this.elem;
+  };
+
+  tableFiller() {
+    // Создание элементов tHead и tBody внутри table
+    let tHead = this.elem.createTHead();
+    let tBody = this.elem.createTBody();
+
+    // Заполнение tHead по заданному шаблону
+    tHead.innerHTML = `
+    <tr>
+       <th>Имя</th>
+       <th>Возраст</th>
+       <th>Зарплата</th>
+       <th>Город</th>
+       <th></th>
+    </tr>`;
+
+    // Заполнение tBody элементами массива rows и добавление button последним td
+    for (const row of this.rows) {
+      tBody.innerHTML += `
+      <tr>
+        <td>${row.name}</td>
+        <td>${row.age}</td>
+        <td>${row.salary}</td>
+        <td>${row.city}</td>
+        <td><button>X</button></td>
+      </tr>
+      `;
+    };
+
+    // Собираем все кнопки в переменную button и циклом проходим по каждой
+    const buttons = this.elem.querySelectorAll('button');
+
+    for (const button of buttons) {
+      button.addEventListener('click', (e) => {
+        // Удаляем tr, в котором находится button при событии click
+        e.target.parentNode.parentNode.remove();
+      })
+    };
   }
 }
